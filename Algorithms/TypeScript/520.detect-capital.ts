52cf57fe7708157b6a45edad085ca8779aed5bc8
momospnr <h.tagawa@momoirospanner.app>
2022-01-24 15:53:09 +0900

    Update 520.detect-capital.ts

--- Algorithms/TypeScript/520.detect-capital.ts
+++ Algorithms/TypeScript/520.detect-capital.ts
@@ -6,7 +6,40 @@
 
 // @lc code=start
 function detectCapitalUse(word: string): boolean {
+  const wordArr = [...word];
 
+  let firstWordIsCapital: boolean;
+  let secondWordIsCapital: boolean;
+  let ans: boolean = true;
+
+  for (let i = 0; i < wordArr.length; i++) {
+    if (!ans) {
+      break;
+    }
+
+    if (i === 0) {
+      firstWordIsCapital = /[A-Z]/.test(wordArr[i]);
+      continue;
+    }
+
+    if (firstWordIsCapital && i === 1) {
+      secondWordIsCapital = /[A-Z]/.test(wordArr[i]);
+    }
+
+    if (!firstWordIsCapital && secondWordIsCapital) {
+      ans = false;
+      continue;
+    }
+
+    if (firstWordIsCapital && secondWordIsCapital) {
+      ans = /[A-Z]/.test(wordArr[i]);
+      continue;
+    }
+
+    ans = /[a-z]/.test(wordArr[i]);
+  }
+
+  return ans;
 };
 // @lc code=end
 
