90dd2c23255e54edb6baa34af72e335eb08d2a5a
momospnr <h.tagawa@momoirospanner.app>
2022-01-25 14:51:56 +0900

    941.valid-mountain-array.ts

---
+++ Algorithms/TypeScript/941.valid-mountain-array.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=941 lang=typescript
+ *
+ * [941] Valid Mountain Array
+ */
+
+// @lc code=start
+function validMountainArray(arr: number[]): boolean {
+
+};
+// @lc code=end
+
