495bfcf8a890651772417b0c3ed8bc511bcdaae7
momospnr <h.tagawa@momoirospanner.app>
2022-01-25 14:46:17 +0900

    Create 1920.build-array-from-permutation.ts

---
+++ Algorithms/TypeScript/1920.build-array-from-permutation.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1920 lang=typescript
+ *
+ * [1920] Build Array from Permutation
+ */
+
+// @lc code=start
+function buildArray(nums: number[]): number[] {
+
+};
+// @lc code=end
+
