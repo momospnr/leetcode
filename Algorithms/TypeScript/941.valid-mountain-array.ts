79c9384c81bee845971ed7c33b31fbec3f46a2bf
momospnr <h.tagawa@momoirospanner.app>
2022-01-25 16:11:46 +0900

    Update 941.valid-mountain-array.ts

--- Algorithms/TypeScript/941.valid-mountain-array.ts
+++ Algorithms/TypeScript/941.valid-mountain-array.ts
@@ -6,7 +6,41 @@
 
 // @lc code=start
 function validMountainArray(arr: number[]): boolean {
+  if (arr.length < 3) {
+    return false;
+  }
 
+  const state = {
+    increasing: true,
+    decreasing: false,
+    increased: false,
+    decreased: false
+  };
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] === arr[i+1]) {
+      return false
+    }
+
+    if (state.increasing && arr[i] < arr[i+1]) {
+      state.increased = true;
+    }
+
+    if (state.increasing && arr[i] > arr[i+1]) {
+      state.increasing = false;
+      state.decreasing = true;
+    }
+
+    if (state.decreasing && arr[i] > arr[i+1]) {
+      state.decreased = true;
+    }
+
+    if (state.decreasing && arr[i] < arr[i+1]) {
+      return false;
+    }
+  }
+
+  return state.increased && state.decreased;
 };
 // @lc code=end
 
