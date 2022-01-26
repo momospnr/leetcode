094678943a6548677a6133dda5e78d342f79e73a
momospnr <h.tagawa@momoirospanner.app>
2022-01-26 12:22:51 +0900

    Update 1305.all-elements-in-two-binary-search-trees.ts

--- Algorithms/TypeScript/1305.all-elements-in-two-binary-search-trees.ts
+++ Algorithms/TypeScript/1305.all-elements-in-two-binary-search-trees.ts
@@ -19,8 +19,46 @@
  * }
  */
 
-function getAllElements(root1: TreeNode | null, root2: TreeNode | null): number[] {
+interface TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
 
+function getAllElements(root1: TreeNode | null, root2: TreeNode | null): number[] {
+  const arr = [];
+
+  if (root1) {
+    checkTreeNode(arr, root1);
+  }
+
+  if (root2) {
+    checkTreeNode(arr, root2);
+  }
+
+  return arr.sort((a, b) => a - b);
 };
+
+function checkTreeNode(arr: number[], node: TreeNode) {
+  arr.push(node.val);
+
+  if (typeof node.left === 'number') {
+    arr.push(node.left);
+  }
+
+  if (node.left && typeof node.left === 'object') {
+    checkTreeNode(arr, node.left);
+  }
+
+  if (typeof node.right === 'number') {
+    arr.push(node.right);
+  }
+
+  if (node.right && typeof node.right === 'object') {
+    checkTreeNode(arr, node.right);
+  }
+
+  return;
+}
 // @lc code=end
 
