5bd5b6589600870a9bea055a5993c2e3b30b4e0b
momospnr <h.tagawa@momoirospanner.app>
2022-01-31 18:35:01 +0900

    Create 1480.running-sum-of-1-d-array.ts

---
+++ Algorithms/TypeScript/1480.running-sum-of-1-d-array.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1480 lang=typescript
+ *
+ * [1480] Running Sum of 1d Array
+ */
+
+// @lc code=start
+function runningSum(nums: number[]): number[] {
+
+};
+// @lc code=end
+
