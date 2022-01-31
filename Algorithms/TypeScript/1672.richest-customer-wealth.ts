78db5d736f28b4621e871eef40f2eecee58a257b
momospnr <h.tagawa@momoirospanner.app>
2022-01-31 16:40:57 +0900

    Create 1672.richest-customer-wealth.ts

---
+++ Algorithms/TypeScript/1672.richest-customer-wealth.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1672 lang=typescript
+ *
+ * [1672] Richest Customer Wealth
+ */
+
+// @lc code=start
+function maximumWealth(accounts: number[][]): number {
+
+};
+// @lc code=end
+
