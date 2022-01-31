91c0ae9d05d60fac1ce99188db719921275ae5bb
momospnr <h.tagawa@momoirospanner.app>
2022-01-31 18:49:19 +0900

    Update 2011.final-value-of-variable-after-performing-operations.ts

--- Algorithms/TypeScript/2011.final-value-of-variable-after-performing-operations.ts
+++ Algorithms/TypeScript/2011.final-value-of-variable-after-performing-operations.ts
@@ -6,7 +6,17 @@
 
 // @lc code=start
 function finalValueAfterOperations(operations: string[]): number {
+  let X: number = 0;
+  for (const operation of operations) {
+    if (operation.indexOf("++") > -1) {
+      X++;
+    }
 
+    if (operation.indexOf("--") > -1) {
+      X--;
+    }
+  }
+  return X;
 };
 // @lc code=end
 
