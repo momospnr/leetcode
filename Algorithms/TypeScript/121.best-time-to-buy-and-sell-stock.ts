715cc0b6b128c757db0470ede43ca81d1fad5ecf
momospnr <h.tagawa@momoirospanner.app>
2022-02-01 17:52:05 +0900

    Create 121.best-time-to-buy-and-sell-stock.ts

---
+++ Algorithms/TypeScript/121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=121 lang=typescript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+
+};
+// @lc code=end
+
