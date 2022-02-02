91f3389758bc63d44a2537c7edef85c285ffb221
momospnr <h.tagawa@momoirospanner.app>
2022-02-02 10:10:55 +0900

    438.find-all-anagrams-in-a-string.ts

---
+++ Algorithms/TypeScript/438.find-all-anagrams-in-a-string.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=438 lang=typescript
+ *
+ * [438] Find All Anagrams in a String
+ */
+
+// @lc code=start
+function findAnagrams(s: string, p: string): number[] {
+
+};
+// @lc code=end
+
