beeb99e05d1b187e2618c733db2a6041616acdfd
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 15:04:57 +0900

    Update 2114.maximum-number-of-words-found-in-sentences.ts

--- Algorithms/TypeScript/2114.maximum-number-of-words-found-in-sentences.ts
+++ Algorithms/TypeScript/2114.maximum-number-of-words-found-in-sentences.ts
@@ -6,6 +6,14 @@
 
 // @lc code=start
 function mostWordsFound(sentences: string[]): number {
+  let max = 0;
+
+  sentences.map((sentence: string) => {
+    const len = sentence.split(" ").length;
+    max = Math.max(max, len);
+  });
+
+  return max;
 
 };
 // @lc code=end
