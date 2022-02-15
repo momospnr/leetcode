a0169d1f9b3c45012876d9e32e1f4643a563acc5
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 15:01:19 +0900

    Create 2114.maximum-number-of-words-found-in-sentences.ts

---
+++ Algorithms/TypeScript/2114.maximum-number-of-words-found-in-sentences.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=2114 lang=typescript
+ *
+ * [2114] Maximum Number of Words Found in Sentences
+ */
+
+// @lc code=start
+function mostWordsFound(sentences: string[]): number {
+
+};
+// @lc code=end
+
