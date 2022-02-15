5b390c4c06dc3fb360d506a133955f1d8e21f87a
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 15:22:59 +0900

    Create 1476.subrectangle-queries.ts

---
+++ Algorithms/TypeScript/1476.subrectangle-queries.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1476 lang=typescript
+ *
+ * [1476] Subrectangle Queries
+ */
+
+// @lc code=start
+class SubrectangleQueries {
+    constructor(rectangle: number[][]) {
+
+    }
+
+    updateSubrectangle(row1: number, col1: number, row2: number, col2: number, newValue: number): void {
+
+    }
+
+    getValue(row: number, col: number): number {
+
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * var obj = new SubrectangleQueries(rectangle)
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+ * var param_2 = obj.getValue(row,col)
+ */
+// @lc code=end
+
