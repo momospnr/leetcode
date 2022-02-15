8cc07c59ceae5be0d44c75b72809b22aac6caabc
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 18:23:29 +0900

    Create 1512.number-of-good-pairs.ts

---
+++ Algorithms/TypeScript/1512.number-of-good-pairs.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1512 lang=typescript
+ *
+ * [1512] Number of Good Pairs
+ */
+
+// @lc code=start
+function numIdenticalPairs(nums: number[]): number {
+
+};
+// @lc code=end
+
