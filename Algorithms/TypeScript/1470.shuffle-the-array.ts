1fc1573899324e139ccb5f6eeb4f83cf096d034d
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 15:21:25 +0900

    Update 1470.shuffle-the-array.ts

--- Algorithms/TypeScript/1470.shuffle-the-array.ts
+++ Algorithms/TypeScript/1470.shuffle-the-array.ts
@@ -6,7 +6,13 @@
 
 // @lc code=start
 function shuffle(nums: number[], n: number): number[] {
+  const ans: number[] = [];
 
+  for (let i = 0; i < n; i++) {
+    ans.push(nums[i], nums[i+n]);
+  }
+
+  return ans;
 };
 // @lc code=end
 
