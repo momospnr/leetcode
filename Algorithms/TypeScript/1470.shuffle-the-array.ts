0e21dc43dfcf0c967b32c5405e813f2ed8c0ecc6
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 15:20:50 +0900

    Create 1470.shuffle-the-array.ts

---
+++ Algorithms/TypeScript/1470.shuffle-the-array.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1470 lang=typescript
+ *
+ * [1470] Shuffle the Array
+ */
+
+// @lc code=start
+function shuffle(nums: number[], n: number): number[] {
+
+};
+// @lc code=end
+
