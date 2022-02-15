679b7835ba2874adaadc8965812c8d9d832e4169
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 14:59:20 +0900

    Update 136.single-number.ts

--- Algorithms/TypeScript/136.single-number.ts
+++ Algorithms/TypeScript/136.single-number.ts
@@ -6,7 +6,21 @@
 
 // @lc code=start
 function singleNumber(nums: number[]): number {
+  const map = new Map();
+  nums.forEach((num: number) => {
+    if (!map.has(num)) {
+      map.set(num, 1);
+    } else {
+      map.delete(num);
+    }
 
+  });
+
+  for (const iterator of map) {
+    if (iterator[1] === 1) {
+      return iterator[0];
+    }
+  }
 };
 // @lc code=end
 
