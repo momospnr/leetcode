3c9a883bffe076602c99e60b790f3f082cd4be78
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 18:22:38 +0900

    Update 1476.subrectangle-queries.ts

--- Algorithms/TypeScript/1476.subrectangle-queries.ts
+++ Algorithms/TypeScript/1476.subrectangle-queries.ts
@@ -6,16 +6,22 @@
 
 // @lc code=start
 class SubrectangleQueries {
-    constructor(rectangle: number[][]) {
+    private rectangle: number[][];
 
+    constructor(rectangle: number[][]) {
+      this.rectangle = rectangle;
     }
 
     updateSubrectangle(row1: number, col1: number, row2: number, col2: number, newValue: number): void {
-
+      for (let i = row1; i <= row2; i++) {
+        for (let j = col1; j <= col2; j++) {
+          this.rectangle[i][j] = newValue;
+        }
+      }
     }
 
     getValue(row: number, col: number): number {
-
+      return this.rectangle[row][col];
     }
 }
 
