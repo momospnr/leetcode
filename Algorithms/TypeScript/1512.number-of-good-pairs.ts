626ee9617aba6183102b85e581cb664c62a246d3
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 18:36:31 +0900

    Update 1512.number-of-good-pairs.ts

--- Algorithms/TypeScript/1512.number-of-good-pairs.ts
+++ Algorithms/TypeScript/1512.number-of-good-pairs.ts
@@ -6,7 +6,16 @@
 
 // @lc code=start
 function numIdenticalPairs(nums: number[]): number {
+  let ans = 0;
 
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+     if (nums[i] === nums[j]) {
+       ans++;
+     }
+    }
+  }
+  return ans;
 };
 // @lc code=end
 
