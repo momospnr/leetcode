474ec738a066bf767d7ff2706af1c110bf4960b8
momospnr <h.tagawa@momoirospanner.app>
2022-02-15 15:06:31 +0900

    Create 1108.defanging-an-ip-address.ts

---
+++ Algorithms/TypeScript/1108.defanging-an-ip-address.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1108 lang=typescript
+ *
+ * [1108] Defanging an IP Address
+ */
+
+// @lc code=start
+function defangIPaddr(address: string): string {
+
+};
+// @lc code=end
+
