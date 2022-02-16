81d3ae114e1ef70173cf3ee26ca824f0ade95cea
momospnr <h.tagawa@momoirospanner.app>
2022-02-16 10:50:56 +0900

    Update 24.swap-nodes-in-pairs.ts

--- Algorithms/TypeScript/24.swap-nodes-in-pairs.ts
+++ Algorithms/TypeScript/24.swap-nodes-in-pairs.ts
@@ -16,8 +16,27 @@
  *     }
  * }
  */
+interface ListNode {
+  val: number;
+  next: ListNode | null;
+}
 
 function swapPairs(head: ListNode | null): ListNode | null {
+  if (head?.val !== 0 && ! head?.val || ! head?.next) {
+    return head;
+  }
+
+  const val = head.val;
+  const nextVal = head.next.val;
+
+  head.val = nextVal;
+  head.next.val = val;
+
+  if (head.next.next) {
+    swapPairs(head.next.next);
+  }
+
+  return head;
 
 };
 // @lc code=end
