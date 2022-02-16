45855776cda93c621f945feb708a5618de2e5b7b
momospnr <h.tagawa@momoirospanner.app>
2022-02-16 09:52:42 +0900

    Create 24.swap-nodes-in-pairs.ts

---
+++ Algorithms/TypeScript/24.swap-nodes-in-pairs.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=24 lang=typescript
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapPairs(head: ListNode | null): ListNode | null {
+
+};
+// @lc code=end
+
