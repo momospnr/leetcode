303e8561e61e096551c93e759de4290213014a11
momospnr <h.tagawa@momoirospanner.app>
2022-02-21 18:45:14 +0900

    Create 1689.partitioning-into-minimum-number-of-deci-binary-numbers.ts

---
+++ Algorithms/TypeScript/1689.partitioning-into-minimum-number-of-deci-binary-numbers.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1689 lang=typescript
+ *
+ * [1689] Partitioning Into Minimum Number Of Deci-Binary Numbers
+ */
+
+// @lc code=start
+function minPartitions(n: string): number {
+
+};
+// @lc code=end
+
