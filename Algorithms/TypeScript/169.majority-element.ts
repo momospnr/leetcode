99122991579f8c707bc80d591094113c488936ae
momospnr <h.tagawa@momoirospanner.app>
2022-02-21 18:05:29 +0900

    Update 169.majority-element.ts

--- Algorithms/TypeScript/169.majority-element.ts
+++ Algorithms/TypeScript/169.majority-element.ts
@@ -6,7 +6,28 @@
 
 // @lc code=start
 function majorityElement(nums: number[]): number {
+  const map = new Map();
+  const obj = {
+    max: {
+      key: 0,
+      value: 0
+    }
+  };
 
+  nums.forEach((num: number) => {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
+    }
+
+    if (obj.max.value < map.get(num) ) {
+      obj.max.key = num;
+      obj.max.value = map.get(num);
+    }
+  });
+
+  return obj.max.key;
 };
 // @lc code=end
 
